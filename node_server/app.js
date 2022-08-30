82f4b0bbca4c0e51b6a1a74840e1e2ca805a718e
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2022-08-30 16:25:45 +0530

    makechanges

---
+++ node_server/app.js
@@ -0,0 +1,21 @@
+//This is node server js file
+
+//The main idea behind Socket.IO is that you can send and receive any events you want, with any data you want. Any objects that can be encoded as JSON will do, and binary data is supported too.`
+
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+
+// app.get('/', (req, res) => {
+//   res.status(200).render('../index.html')
+// });
+
+// server.listen(3000, () => {
+//   console.log('listening on port 3000');
+// });
+
+
+// Express initializes app to be a function handler that you can supply to an HTTP server (as seen in line 4).
+// We define a route handler / that gets called when we hit our website home.
+// We make the http server listen on port 3000
