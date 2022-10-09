81cd8fcdc1c963bd87aa4f051139a8575305e316
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2022-10-09 18:20:47 +0530

    Handled some server side problems

--- node_server/app.js
+++ node_server/app.js
@@ -1,23 +1,34 @@
-//This is node server js file
+//This is node server js file it will handle all the connections
 
-//The main idea behind Socket.IO is that you can send and receive any events you want, with any data you want. Any objects that can be encoded as JSON will do, and binary data is supported too.`
+//The main idea behind Socket.IO is that you can send and receive any events you want, with any data you want.
+//Any objects that can be encoded as JSON will do, and binary data is supported too.`
+const httpServer = require("http").createServer();
+const io = require('socket.io')(8000,httpServer,{
+  cors: {
+    origin: 'http://localhost:5500',
+    method: ['GET', 'POST']
+  }
+}) //This server(io) is the instance of socket.io, it will listen all the events which are emmited 
+const users = {}
+//when the connection is made all the coustom events are listened in it 
+io.on('connection', socket=>{ //io.on will handle all the events(new connections i.e. new user, new message by the user)  here it means when connection comes arrow func. runs having socket as parameter
 
-// const express = require('express');
-// const app = express();
-// const http = require('http');
-// const server = http.createServer(app);
+  //New connection is made 
+  socket.on('new-user-joined', name=>{ //socket will handle all coustomized events in clinet.js
+    console.log('user name: ' + name)
+    users[socket.id] = name; 
+    socket.broadcast.emit('User-Joined', name)  //This will broadcast the message
+  })
+  //Message handling
+  // socket.on('send', message=>{
+  //   socket.broadcast.emit('receive', {message:message, name:users[socket.id]}) //this will broadcast the object having name, message
+  // })
 
-// app.get('/', (req, res) => {
-//   res.status(200).render('../index.html')
-// });
+})
 
-// server.listen(3000, () => {
-//   console.log('listening on port 3000');
-// });
+io.on('send', message=>{ //io will handle all the events
+    socket.broadcast.emit('receive', {message:message, name: users[socket.id]})  //This will broadcast the  coustomized message
+})
 
 
 
-
-// Express initializes app to be a function handler that you can supply to an HTTP server (as seen in line 4).
-// We define a route handler / that gets called when we hit our website home.
-// We make the http server listen on port 3000
