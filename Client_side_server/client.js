f70c23ad5ff298ae19f43dd09972f3f0864657f2
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2023-06-06 12:22:15 +0530

    all problems resolved

--- Client_side_server/client.js
+++ Client_side_server/client.js
@@ -1,3 +1,4 @@
+
 const socket = io('http://localhost:8000', {
     cors: {
         "Access-Control-Request-Origin": ["http://127.0.0.1:8000"]
@@ -7,31 +8,40 @@ const socket = io('http://localhost:8000', {
     // withCredentials: true,
     // "Content-type": "text/javascript"
 })
-const messageContainer_send = document.querySelector('div.text-send')
-const messageContainer_receive = document.querySelector('div.text-receive')
+const totalMessages = document.querySelector('.container')
+const messageContainer_send = document.querySelector('div.send')
+const messageContainer_receive = document.querySelector('div.receive')
 const messageContainer_enter = document.querySelector('div.enter')
 const messageContainer_leave = document.querySelector('div.leave')
 const data = document.getElementById("send")
 const messagesend = document.getElementById('message')
 var sound = new Audio('ting.mp3')
-function append(message, message_type) {
+function append(message, user_name, message_type) {
 
     if (message_type == 'text-send') {
         //creation
+        const messageEle = document.createElement('div');
+        messageEle.classList.add('text');
+        messageEle.classList.add('send');        
         const para = document.createElement('p')
         const text = document.createTextNode(message)
         //appending
         para.appendChild(text)
-        messageContainer_send.appendChild(para);
+        messageEle.append(para);
+        totalMessages.append(messageEle);
     }
     if (message_type == 'text-receive') {
         //creation
-
+        const messageEle = document.createElement('div');
+        messageEle.classList.add('text');
+        messageEle.classList.add('receive');        
         const para = document.createElement('p')
-        const text = document.createTextNode(message)
+        const text = document.createTextNode(user_name +":"+ message)
         //appending
         para.appendChild(text)
-        messageContainer_receive.appendChild(para);
+        messageEle.append(para);
+        totalMessages.append(messageEle);
+
         sound.play()
     }
     if (message_type == 'enter') {
@@ -52,30 +62,32 @@ function append(message, message_type) {
     }
 
 }
+const namee = prompt("Enter your Name: ")
 
 data.addEventListener('submit', (e)=>{
     e.preventDefault() //this prevents from reloading the page
+    
     const message = messagesend.value;
-    append(message, 'text-send') //To know what we have send
+    append(message, namee, 'text-send') //To know what we have send
     socket.emit('send', message) //To send the message to others
     messagesend.value = ''
 })
 
-const name = prompt("Enter your Name: ")
+
 
 //socket.emit will send messaage to server
-socket.emit('new-user', name) //this is custom event made and emmited which is listened on app.js (calling of event and on server it will receive and then call to client which is below received)
+socket.emit('new-user', namee) //this is custom event made and emmited which is listened on app.js (calling of event and on server it will receive and then call to client which is below received)
 
 //socket.on will listen incoming data. 
 socket.on('user-joined', name => {
-    console.log("hello is there anyone")
-    append(`${name} joined the chat`, 'enter')
+    append(`${name} joined the chat`, '', 'enter')
 })
-socket.on('reveive', (obj)=>{ //an object containing message and users are received from server
-    append(obj.message, 'text-receive')
+socket.on('receive', (obj)=>{ //an object containing message and users are received from server
+    console.log(obj.message+ 'this is text message send');
+    append(obj.message, obj.name, 'text-receive')
 })
 socket.on('leave', name=>{
-    append(`${name} left the chat`, 'leave')
+    append(`${name} left the chat`, "" + 'leave')
 })
 
 
